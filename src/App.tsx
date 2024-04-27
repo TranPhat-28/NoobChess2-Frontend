db397efa8fc40d52338f5cb146daf79ee20429a8
TranPhat-28 <tphat8301@gmail.com>
2024-04-28 02:12:21 +0700

    feat: Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+// import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
+
+import { Route, Routes } from "react-router-dom";
+
+// Pages
+import LoginPage from "./pages/Login";
 
 function App() {
-    // const login = useGoogleLogin({
-    //     onSuccess: (tokenResponse) => console.log(tokenResponse),
-    //     flow: 'implicit',
-    // });
-
     return (
-        <>
-            {/* <button className="btn btn-secondary" onClick={() => login()}>
-                Google login
-            </button> */}
-            <GoogleLogin
+        // <Routes>
+        //     <Route path="/" element={<RootLayout />}>
+        //         <Route index element={<Home />} />
+        //         <Route path="/lobby" element={<Lobby />} />
+        //         <Route path="/news" element={<News />} />
+        //         <Route path="/social" element={<Social />} />
+        //         <Route path="/settings" element={<Settings />} />
+        //     </Route>
+
+        //     <Route path="/game" element={<GameLayout />}>
+        //         <Route path=":mode" element={<Game />} />
+        //     </Route>
+
+        //     <Route path="*" element={<div>Not found</div>} />
+        // </Routes>
+
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/main">
+                <Route index element={<div>Lobby</div>} />
+                <Route path="social" element={<div>Social</div>} />
+            </Route>
+            <Route path="/quickplay" element={<div>Quick play</div>} />
+        </Routes>
+    );
+}
+
+export default App;
+
+{
+    /* <GoogleLogin
                 onSuccess={(credentialResponse) => {
                     console.log(credentialResponse);
                 }}
@@ -20,9 +46,5 @@ function App() {
                 }}
                 type="icon"
                 shape="circle"
-            />
-        </>
-    );
+            /> */
 }
-
-export default App;
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,42 @@
+import { FaChessKing, FaFacebookF } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="custom-bg h-full w-full object-fill flex items-center justify-center p-6">
+            <div className="bg-base-100 p-4 flex flex-col items-center rounded-lg w-full max-w-lg shadow-md">
+                <img src="/icon512.png" alt="logo" className="w-28 mt-4" />
+                <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-0">
+                    Welcome to Noob Chess
+                </h1>
+                <p>The chess platform made by noob</p>
+
+                <h2 className="mt-10 p-2">Login to save your progress</h2>
+                <div className="w-full max-w-xs flex flex-col gap-2">
+                    <button className="btn btn-outline w-full p-2 text-center">
+                        <FcGoogle />
+                        Continue with Google
+                    </button>
+                    <button className="btn btn-outline w-full p-2 text-center">
+                        <FaFacebookF color="blue" />
+                        Continue with Facebook
+                    </button>
+                    <div className="divider">OR</div>
+
+                    <button
+                        className="btn btn-primary h-20"
+                        onClick={() => navigate("/quickplay")}
+                    >
+                        <FaChessKing />
+                        Play a quick game
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
